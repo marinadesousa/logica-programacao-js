8d547c1afbba158508aa0c4af5e8903d0a9b3196
Marina de Sousa <160603203+marinadesousa@users.noreply.github.com>
2024-03-19 14:33:39 +0000

    Concluindo desafio3.js

--- desafio3.js
+++ desafio3.js
@@ -1,9 +1,24 @@
-const temperaturaAtual = window.prompt ('Digite a temperatura atual:');
-const escalaAtual = window.prompt ('Faça a escolha da escala atual');
-const conversao = window.prompt ('Digite a temperatura que será ser convertida');
-let calculo
+const temperatura = parseFloat(window.prompt ('Digite a temperatura atual:'));
+const escalaAtual = parseFloat(window.prompt ('Faça a escolha da escala atual:')).toUpperCase();
+const conversao = parseFloat(window.prompt ('Digite a temperatura que será ser convertida (F , C, K):')).toUpperCase();
 
-if (escalaAtual=== 'C' && conversao === 'F') {
-    calculo = ( temperaturaAtual * 9/5) + 32
-    window.alert (`Celsius para Fahrenheit: ${calculo}?F`)
+
+if (escalaAtual == "C" && conversao == "F") {
+        calculo = (temperatura * 9/5) + 32;
+        window.alert(`Celsius para Fahrenheit: ${calculo} °F`);
+}else if ( escalaAtual == 'C' && conversao == 'K') { 
+    calculo = (temperatura - 273.15);
+    window.alert(`Celsius para Kelvin: ${calculo} °K`);
+}else if ( escalaAtual == 'F' && conversao == 'C') {
+    calculo = (temperatura - 32) * 5/9;
+    window.alert(`Fahrenheit para Celsius: ${calculo} °C`);
+} else if (escalaAtual == 'F' && conversao == 'K'){
+    calculo = ((temperatura * 1) + 459.67) * 5/9;
+    window.alert(`Fahrenheit para Kelvin: ${calculo} °K`);
+} else if (escalaAtual == 'K' && conversao == 'C'){
+    calculo = (temperatura * 1) - 273.15;
+    window.alert(`Kelvin para Celsius: ${calculo} °C`);   
+} else if (escalaAtual == 'K' && conversao == 'F'){
+    calculo = (temperatura * 1) * 9/5 - 459.67;
+    window.alert(`Kelvin para Fahrenheit: ${calculo} °F`);
 }
