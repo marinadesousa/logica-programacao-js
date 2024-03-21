1b17a2cfed2c2c22ad940dad2cd5e8e5dbd37cf5
marinadesousa <sousamarina224@gmail.com>
2024-03-21 11:49:03 -0300

    Criando funçoes

--- desafio2.js
+++ desafio2.js
@@ -1,27 +1,25 @@
 // declaração sem atribuição (só com let)
-function calculadoraIMC() {
-    let altura, peso;
-const imc = peso / (altura**2)
+function calculaIMC(peso, altura) {
+
+     const imc = peso / altura **2;
+     let mensagem;
 
     // teste de mesa
-    imc = 19.1 ;
+    // imc = 19.1 ;
 
     if (imc <= 18.5) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Abaixo do peso`);
-            
+        mensagem = `IMC: ${imc.toFixed(1)} --> Abaixo do peso`;        
     } else if (imc < 25) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Peso normal`);
-    
+        win`IMC: ${imc.toFixed(1)} --> Peso normal`;
     } else if (imc < 30) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Sobrepeso`);
-    
+        mensagem = `IMC: ${imc.toFixed(1)} --> Sobrepeso`;
     } else if (imc < 35) {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau I`);
-    
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau I`;
     } else if (imc < 40 )  {
-        window.alert(`IMC: ${imc.toFixed(1)} --> Obesidade grau II`);
+        mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau II`;
     } else {
-        window.alert(`IMC: ${imc} -- Obesidade grau III`);
+        mensagem = `IMC: ${imc} -- Obesidade grau III`;
     }
 
+    return mensagem;
 }
