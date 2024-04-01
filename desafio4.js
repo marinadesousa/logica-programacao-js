89d434fb25d1e08595c3e59c10128f3946416c7a
marinadesousa <sousamarina224@gmail.com>
2024-04-01 11:45:25 -0300

    Novos exercicios

---
+++ desafio4.js
@@ -0,0 +1,11 @@
+let resultado 
+
+function tipoTriangulo ( lado A, lado B, lado C) {
+    if(lado A === lado B & lado B === lado C) {
+        return "Equilátero";
+    } else if ( ) {
+        return "";
+    } else {
+        return "" ;
+    }
+}
