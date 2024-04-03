da4813724624bb706b57fe3d52f85ae381f8f828
marinadesousa <sousamarina224@gmail.com>
2024-04-03 11:50:14 -0300

    Estilização

---
+++ Exercícios complementares/exercicio1.js
@@ -0,0 +1,13 @@
+let resultado;
+
+function verificarTriangulo (ladoA, LadoB, LadoC) {
+    if (ladoA === LadoB && LadoB === LadoC) {
+        return "Equilátero";
+    } else if (ladoA === LadoB || ladoA === LadoC || LadoB === LadoC) {
+        return "Isósceles";
+    } else if (ladoA != LadoB && ladoA != LadoC && LadoB && LadoC) {
+        return "Escaleno";
+    } else {
+       erro = "Essas medidas não são validas";
+    }
+}
